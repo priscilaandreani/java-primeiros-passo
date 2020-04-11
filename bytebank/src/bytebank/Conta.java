0dd07a46472d02092d5e0a8d546d8f68e899cabf
Priscila Andreani <priscilaandreani@gmail.com>
2020-04-11 16:41:36 -0300

    Adiciona método a Conta

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -1,8 +1,15 @@
 package bytebank;
 
 public class Conta {
+	//atributos do objeto conta
 	double saldo;
 	int agencia;
 	int numero;
 	String titular;
+	
+	//métodos (o que eu posso fazer com a conta)
+	public void deposita(double valor) /* void não tem resposta */ {
+		//this especifica o atributo do método que está sendo invocado
+		this.saldo = saldo + valor;
+	}
 }
---
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,10 @@
+package bytebank;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		// Váriavel classe 
+		Conta contaTemporaria = new Conta();
+		contaTemporaria.saldo = 200;
+		contaTemporaria.deposita(100);
+	}
+}
