dfe848c3497e81c9b73ea445c177aacb9fac3430
Priscila Andreani <priscilaandreani@gmail.com>
2020-04-11 18:44:31 -0300

    Adiciona método saca e tranfere

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -12,4 +12,23 @@ public class Conta {
 		//this especifica o atributo do método que está sendo invocado
 		this.saldo = saldo + valor;
 	}
+	
+	public boolean saca (double valor) {
+		if (this.saldo >= valor ) {
+			this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.saldo += valor; // ou destino.deposita(valor)
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -6,5 +6,22 @@ public class TestaMetodo {
 		Conta contaTemporaria = new Conta();
 		contaTemporaria.saldo = 200;
 		contaTemporaria.deposita(100);
+		
+		boolean conseguiuSacar = contaTemporaria.saca(20);
+		System.out.println(contaTemporaria.saldo);
+		System.out.println("A operação foi realizada: "+ conseguiuSacar);
+		
+		Conta contaRica = new Conta();
+		contaRica.saldo = 2000;
+		
+		boolean transferencia = contaRica.transfere(2200, contaTemporaria);
+		System.out.println(contaRica.saldo);
+		System.out.println(contaTemporaria.saldo);
+		
+		if(transferencia) {
+			System.out.println("Transferência realizada com sucesso!");
+		} else {
+			System.out.println("Não foi possível realizar a trasnferência. Saldo insuficiente.");
+		}
 	}
 }
