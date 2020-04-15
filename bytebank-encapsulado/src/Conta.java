9252f29301de5c3925360896b402bc33a77b6541
Priscila Andreani <priscilaandreani@gmail.com>
2020-04-15 08:30:27 -0300

    Adiciona encapsulamento de classes

---
+++ bytebank-encapsulado/src/Conta.java
@@ -0,0 +1,54 @@
+
+
+public class Conta {
+	//atributos do objeto conta
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	
+	//métodos (o que eu posso fazer com a conta)
+	public void deposita(double valor) /* void não tem resposta */ {
+		//this especifica o atributo do método que está sendo invocado
+		this.saldo = saldo + valor;
+	}
+	
+	public boolean saca (double valor) {
+		if (this.saldo >= valor ) {
+			this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.saldo += valor; // ou destino.deposita(valor)
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int novoNumero) {
+		this.numero = novoNumero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+}
---
+++ bytebank-encapsulado/src/GetterSetter.java
@@ -0,0 +1,8 @@
+
+public class GetterSetter {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setNumero(3248);
+		System.out.println("O novo número da sua conta é: " + conta.getNumero());
+	}
+}
